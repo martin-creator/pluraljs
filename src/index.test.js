cadef8f083e72846f040b08507811da33a6e89ea
martin-creator <martinlubowa@outlook.com>
2021-08-19 19:51:11 +0200

    This is my first professional project module 9

---
+++ src/index.test.js
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import jsdom from "jsdom";
+import fs from "fs";
+
+
+describe('Our first test', () => {
+  it('should pass',() => {
+    expect(true).to.equal(true);
+
+  })
+
+});
+
+describe('index.html', () => {
+  it('should say Hello Martin Joseph Lubowa!!!!', () =>{
+    const index = fs.readFileSync("./src/index.html","utf-8");
+    const {JSDOM} = jsdom;
+    const dom =  new JSDOM(index);
+    const h1 = dom.window.document.getElementsByName("h1")[0];
+    expect(h1.innerHTML).to.equal("Hello Martin Joseph Lubowa!");
+    dom.window.close();
+
+  })
+
+})
