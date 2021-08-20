838bef50e0157c8e0dc6eb80151482f7791c5683
martin-creator <martinlubowa@outlook.com>
2021-08-20 15:35:00 +0200

    building and deployment

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,19 +1,30 @@
 import path from "path";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-  mode: "development",
-  devtool: "eval-source-map",
-  entry: "./src/index.js",
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
   output: {
-    path: path.resolve(__dirname, "src"),
-    publicPath: "/",
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
   module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
-    ],
-  },
-};
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
