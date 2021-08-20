62f20a65806ae02e7821d3162591617e6287b125
martin-creator <martinlubowa@outlook.com>
2021-08-20 17:49:14 +0200

    Final Deployment

--- src/index.test.js
+++ src/index.test.js
@@ -12,11 +12,11 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say Hello Martin Joseph Lubowa!!!!', () =>{
+  it('should say User', () =>{
     const index = fs.readFileSync("./src/index.html","utf-8");
     const {JSDOM} = jsdom;
     const dom =  new JSDOM(index);
-    const h1 = dom.window.document.getElementsByName("h1")[0];
+    const h1 = dom.window.document.getElementsByTagName("h1")[0];
     expect(h1.innerHTML).to.equal("Users");
     dom.window.close();
 
